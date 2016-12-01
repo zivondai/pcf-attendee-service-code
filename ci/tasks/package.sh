5c4ff61d6199be469d956a9b6a13269346b1bb0b
Trevor Whitney <twhitney@pivotal.io>
2016-12-01 16:21:37 -0700

    Added a CF deployment to the concourse pipeline

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -1,6 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e +x
 
-cd attendee-service
-./mvnw clean package
+pushd attendee-service
+  echo "Resolving Maven dependencies"
+  ./mvnw clean compile > /dev/null
+
+  echo "Running tests"
+  ./mvnw test
+
+  echo "Packaging JAR"
+  ./mvnw package -Dmaven.test.skip=true
+popd
+
+jar_count=`find attendee-service/target -type f -name *.jar | wc -l`
+
+if [ $jar_count -gt 1 ]; then
+  echo "More than one jar found, don't know which one to deploy. Exiting"
+  exit 1
+fi
+
+find attendee-service/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
+
+echo "Contents of package-output"
+ls package-output
+
+echo "Done packaging"
+exit 0

a69bcb9c164da77afed5e2912c10e52947fa1e7a
Trevor Whitney <twhitney@pivotal.io>
2016-12-20 09:22:15 -0700

    fix more paths

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -7,14 +7,14 @@ pushd attendee-service-source
   ./mvnw clean package -DskipTests
 popd
 
-jar_count=`find attendee-service/target -type f -name *.jar | wc -l`
+jar_count=`find attendee-service-source/target -type f -name *.jar | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find attendee-service/target -type f -name *.jar -exec cp "{}" build-artifacts/attendee-service.jar \;
+find attendee-service-source/target -type f -name *.jar -exec cp "{}" build-artifacts/attendee-service.jar \;
 
 echo "Done packaging"
 exit 0

a4980ed0b74932f8b4b568264391ffc7ff58a1e0
Trevor Whitney <twhitney@pivotal.io>
2016-12-20 10:19:04 -0700

    install cf binary for deployment

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -2,10 +2,23 @@
 
 set -e +x
 
+# installtion instructions from https://github.com/cloudfoundry/cli
+# first add the Cloud Foundry Foundation public key and package repository to your system
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+# then, update your local package index, then finally install the cf CLI
+sudo apt-get update
+sudo apt-get install cf-cli
+
+if [ ! `which cf` ]; then
+  echo 'CF Installation Failed'
+  exit 1
+fi
+
 pushd build-artifacts
-  echo "Deploying to Cloud Foundry"
-  cf login -a $CF_API -o $CF_ORG -s $CF_SPACE -u $CF_USER -p $CF_PASSWORD
-  cf push -f manifest.yml -p attendee-service.jar
+echo "Deploying to Cloud Foundry"
+cf login -a $CF_API -o $CF_ORG -s $CF_SPACE -u $CF_USER -p $CF_PASSWORD
+cf push -f manifest.yml -p attendee-service.jar
 popd
 
 attendee_service_url=`cf apps | grep ^attendee-service | awk '{print $6}'`

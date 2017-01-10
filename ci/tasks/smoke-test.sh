049ebbc2837e34afd14270dd65c3f90b7e89d888
Trevor Whitney <twhitney@pivotal.io>
2017-01-10 12:59:20 -0700

    final working smoke tests

--- ci/tasks/smoke-test.sh
+++ ci/tasks/smoke-test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-set -ex
+set -e
+
+apt-get update && apt-get install -y curl
+
+set -x
 
 if [ -z $ATTENDEE_SERVICE_URL ]; then
   echo "ATTENDEE_SERVICE_URL not set"
@@ -9,7 +13,7 @@ fi
 
 pushd attendee-service-source
   echo "Running smoke tests for Attendee Service deployed at $ATTENDEE_SERVICE_URL"
-  smoke_tests/bin/test $ATTENDEE_SERVICE_URL
+  smoke-tests/bin/test $ATTENDEE_SERVICE_URL
 popd
 
 exit 0

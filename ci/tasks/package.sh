9eda778293c7d312da7ae094970be2f76edd4865
Trevor Whitney <twhitney@pivotal.io>
2017-01-09 17:28:11 -0700

    fix package output

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -14,8 +14,7 @@ if [ $jar_count -gt 1 ]; then
   exit 1
 fi
 
-find attendee-service-source/target -type f -name *.jar -exec cp "{}" build-artifacts/attendee-service.jar \;
-cp attendee-service-source/manifest.yml build-artifacts/manifest.yml
+find attendee-service-source/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
 
 echo "Done packaging"
 exit 0

bb668a2e18128a22c6585ddd6148a94dfe694e2a
Amy Bertken <abertken@pivotal.io>
2017-04-10 13:21:05 -0600

    Update package.sh to be like concourse branch

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -1,6 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e +x
 
-cd attendee-service
-./mvnw clean package
+pushd attendee-service-source
+  echo "Packaging JAR"
+  ./mvnw clean package -DskipTests
+popd
+
+jar_count=`find attendee-service-source/target -type f -name *.jar | wc -l`
+
+if [ $jar_count -gt 1 ]; then
+  echo "More than one jar found, don't know which one to deploy. Exiting"
+  exit 1
+fi
+
+find attendee-service-source/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
+
+echo "Done packaging"
+exit 0
